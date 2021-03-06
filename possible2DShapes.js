82ab4bfeeaae4eb6da03dab521e37e31629ec722
Gökçer Yapar <gogolo1991@gmail.com>
2021-03-06 11:43:57 +0300

    Update possible2DShapes.js

--- possible2DShapes.js
+++ possible2DShapes.js
@@ -59,10 +59,70 @@ const possible2DShapes = (shape, foundedShapes, maxUnits) => {
 
 function main(){
     const foundShapes = {};
-    const MAX_UNITS = 4;
+    const MAX_UNITS = 5;
     possible2DShapes([{x: 0, y:0}], foundShapes, MAX_UNITS);
     const shapes = Object.values(foundShapes);
-    return;
+    const shifts = [];
+    for (let i=0; i<shapes.length; i++){
+        const shape = shapes[i];
+        let minX = Number.MAX_SAFE_INTEGER;
+        let minY = Number.MAX_SAFE_INTEGER;
+        for (let j=0; j<shape.length; j++){
+            const point = shape[j];
+            if (minX > point.x) minX = point.x;
+            if (minY > point.y) minY = point.y;
+        }
+        shifts.push({x: minX, y: minY});
+    }
+
+    const shiftedShapes = [];
+    for (let i=0; i<shapes.length; i++){
+        const shape = shapes[i];
+        const shift = shifts[i];
+        const shiftedShape = [];
+        for (let j=0; j<shape.length; j++){
+            const point = {...shape[j]};
+            point.x += Math.abs(shift.x);
+            point.y += Math.abs(shift.y);
+            shiftedShape.push(point);
+        }
+        shiftedShapes.push(shiftedShape);
+    }
+
+    for (let i=0; i<shiftedShapes.length; i++){
+        const shape = [...shiftedShapes[i]];
+        const x = shape[shape.length-1].x-shape[0].x
+        shape.sort((p1, p2) => p1.y === p2.y ? p1.x - p2.x: p1.y - p2.y);
+        const y = shape[shape.length-1].y-shape[0].y
+        shiftedShapes[i].push({x: x, y: y});
+    }
+
+    for (let i=0; i<shiftedShapes.length; i++){
+        const shape = shiftedShapes[i];
+        let line = '';
+        let result = '';
+        let offset = -1;
+        const filterSet = new Set();
+        for (let j=0; j<shape.length-1; j++){
+            if (!filterSet.has(shape[j].x)){
+                filterSet.add(shape[j].x);
+                result += `${line}\n`;
+                line = '';
+                offset = -1;
+            }
+            let spaceCount = shape[j].y - offset -1;
+            offset = shape[j].y;
+            while (spaceCount !== 0){
+                line += ' ';
+                spaceCount--;
+            }
+            line += 'Q';
+            if (j===shape.length-2){
+                result += `${line}\n`;
+            }
+        }
+        console.log(result);
+    }
 }
 
 main();
